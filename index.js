b9630fbc82c2d955a554dfb7bba047d7d7f19230
Sazid60 <shahnawazsazid60@gmail.com>
2024-05-10 11:26:42 +0600

    Book Posting Method Added

--- index.js
+++ index.js
@@ -16,24 +16,42 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
+
+
 async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+
+        // Collection-1
+        // const allBookCollection = client.db('bookDB').collection('books')
+        const librarianCollection = client.db('librarianDB').collection('librarianBooks')
+
+
+        // Add Craft & Art
+        app.post('/add-book', async (req, res) => {
+            const newBook = req.body;
+            console.log(newBook)
+            const result = await librarianCollection.insertOne(newBook);
+            res.send(result)
+        })
+
+
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
 }
 run().catch(console.dir);
 
